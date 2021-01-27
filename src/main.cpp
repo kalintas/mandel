5875a6d042685d6ced9c5ca781abfd456673b449
unuzdaq42 <73061876+unuzdaq42@users.noreply.github.com>
2021-01-27 15:57:51 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include "mandel.h"
 
-#include <iostream>
-
-#include <ImGui/imgui.h>
-
 int main()
 {   
 
-    if(!mandel::Init())
-    {
-        std::cerr << "couldnt init mandel\n";
-        return -1;
-    }
-
+    if(!mandel::Init()) return -1;
+    
     mandel::Run();
 
     return 0;
